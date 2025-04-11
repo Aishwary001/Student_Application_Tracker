a235ad6bf3b499557b9cc802f99f6569af8ccbbe
Aishwary001 <harshrajaparmar17@gmail.com>
2025-04-11 14:55:30 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,11 @@ import Home from './pages/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
 
 // Set the app element for react-modal
 // Modal.setAppElement('#root');
 
-//replace the localhost link to this link "https://cheery-rolypoly-0c0f13.netlify.app/" in stablise the connectivity to the deployed backend
 
 const App = () => {
   return (
