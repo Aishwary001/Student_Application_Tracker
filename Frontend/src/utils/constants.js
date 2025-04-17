b0bfabb1d76138e058e5c104e3a831a7aab22abd
Aishwary001 <harshrajaparmar17@gmail.com>
2025-04-17 21:30:10 +0530

    add backend url

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://studentapplicationtracker-production.up.railway.app/'
+export const BASE_URL = 'https://student-application-tracker.onrender.com'
 // https://student-application-tracker-ochre.vercel.app/
 // https://studentapplicationtracker-production.up.railway.app/
 // export const BASE_URL = 'http://localhost:8000'
+// https://student-application-tracker.onrender.com
