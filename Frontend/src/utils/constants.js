94b40a1892ee6d90e9cae99b4650e00ff6b9997e
Aishwary001 <harshrajaparmar17@gmail.com>
2025-04-17 17:52:56 +0530

    This is manual check2

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://studentapplicationtracker-production.up.railway.app/'
+export const BASE_URL = 'https://studentapplicationtracker-production.up.railway.app/'
 // https://student-application-tracker-ochre.vercel.app/
 // https://studentapplicationtracker-production.up.railway.app/
-export const BASE_URL = 'http://localhost:8000'
+// export const BASE_URL = 'http://localhost:8000'
